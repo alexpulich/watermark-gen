12e329cfa5050a0e67cc2bc05655a3b9a5a493e4
Maxim <totsamax@gmail.com>
2015-05-24 17:45:07 +0300

    Добавил вызов функции изменения прозрачности для картинки.  Для изменения нужно указать правильный класс

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,126 +1,143 @@
-$( document ).ready(function() {
-
-	
-
-	var spinner = $( ".spinner" ).spinner({
-		min: 0
-	});
-
-	var sliderRange = $(".slider-range"),
-		sliderMin = sliderRange.data('min');
-		sliderMax = sliderRange.data('max');
-		sliderStep = sliderRange.data('step')
+$(document).ready(function () {
 
 
-	sliderRange.slider({
-		value: 0,
-		min: sliderMin,
-		max: sliderMax,
-		step: sliderStep,
-		range: 'min'
-	})
 
-	// событие отправки изображения на сервер
- 	$('#file-upload').fileupload({
- 		dataType: 'json',
- 		add: function(e, data) { // отправляем картинку на сервер
- 			data.submit();
- 			console.log('отправляем картинку на сервер');
- 		}
- 	}).bind('fileuploaddone', function (e, data) {
- 		
- 		// выводим картинку в наш документ после события загрузки картинки на сервер
- 		// проверим впервые ли загружается картинка, если нет очистим область вывода
+    var spinner = $(".spinner").spinner({
+        min: 0
+    });
 
- 		if ($('.wraper__image').length > 0 ) {
- 			$('.image-upload').remove();
- 		} else {
- 		// создаем обертку для изображения
- 		console.log('создаем обертку для изображения');
- 			$(".generate__preview").append("<div class='wraper__image'></div>");
- 			$(".wraper__image").append("<div class='wraper__image-bg'></div>");
- 		}
+    var sliderRange = $(".slider-range"),
+        sliderMin = sliderRange.data('min');
+    sliderMax = sliderRange.data('max');
+    sliderStep = sliderRange.data('step');
 
- 		// создаем элемент изображения
- 		var $img = $('<img>', {
-      			src: $("#file-upload").attr("data-url")+"files/"+data.files[0].name, // путь из данных атрибута добачной папки и имя файла из инпута
-     			alt: 'Основное изображение',
-      			title: 'Ваше изображение',
-      			class: 'image-upload' // добавим класс для изображения     
-  			});
-             
-		// ждем загрузки картинки браузером
-		$img.load(function(){
-    		// удаляем атрибуты width и height
-    		$(this).removeAttr("width")
-           .removeAttr("height")
-           .css({ width: "", height: "" });
- 
-    		// получаем  цифры размера изображения
-   	 		var width  = $(this).width();
-    		var height = $(this).height();
 
-    		console.log('width= '+width+' height= '+height);
-    		// и масштабируем его добавочным классом
-    		if (width>height) {
-    			$img.addClass('image-upload-w');
-    			$('.wraper__image-bg').css({'height':''+Math.round(height*653/width)+'px','width':'653px'});
-    	   		} else {
-    			$img.addClass('image-upload-h');
-    			$('.wraper__image-bg').css({'height':'534px','width':''+Math.round(width*534/height)+'px'});
-    		}
-		});
+    sliderRange.slider({
+        value: 0,
+        min: sliderMin,
+        max: sliderMax,
+        step: sliderStep,
+        range: 'min'
+    });
 
-		// добавляем изображение в документ
- 		$(".wraper__image-bg").append($img);
- 		
- 	});
+    // событие отправки изображения на сервер
+    $('#file-upload').fileupload({
+        dataType: 'json',
+        add: function (e, data) { // отправляем картинку на сервер
+            data.submit();
+            console.log('отправляем картинку на сервер');
+        }
+    }).bind('fileuploaddone', function (e, data) {
 
-	// событие отправки #watermark на сервер
- 	$('#watermark').fileupload({
- 		dataType: 'json',
- 		add: function(e, data) { // отправляем картинку на сервер
- 			data.submit();
- 			console.log('отправляем #watermark на сервер');
- 		}
- 	}).bind('fileuploaddone', function (e, data) {
- 		
- 		// проверим есть ли фон, если да загрузим ватемарк
- 		if ($('.wraper__image').length>0) {
+        // выводим картинку в наш документ после события загрузки картинки на сервер
+        // проверим впервые ли загружается картинка, если нет очистим область вывода
 
- 			// удаляем предыдущий вотемарк
- 			if ($('#drag').length>0) {$('#drag').remove();}		
- 			// создаем элемент изображения
-			var $wtm = $('<img>', {
-		  			src: $("#watermark").attr("data-url")+"files/"+data.files[0].name, // путь из данных атрибута добачной папки и имя файла из инпута
-		 			alt: 'Основное изображение',
-		  			title: 'Ваше изображение',
-		  			id: 'drag',
-		  			class: 'image-watermark ui-widget ui-widget-content' // добавим класс для изображения     
-				});
-             
-		
-			// добавляем изображение в документ
-			
-	 		$(".wraper__image-bg").append($wtm);
-	 		//$wtm.wrap('<div id="drag"></div>');
-	 		$("#drag").draggable({ containment: ".image-upload", scroll: false });
- 	}
- 		
- 	});
+        if ($('.wraper__image').length > 0) {
+            $('.image-upload').remove();
+        } else {
+            // создаем обертку для изображения
+            console.log('создаем обертку для изображения');
+            $(".generate__preview").append("<div class='wraper__image'></div>");
+            $(".wraper__image").append("<div class='wraper__image-bg'></div>");
+        }
 
- 	   
+        // создаем элемент изображения
+        var $img = $('<img>', {
+            src: $("#file-upload").attr("data-url") + "files/" + data.files[0].name, // путь из данных атрибута добачной папки и имя файла из инпута
+            alt: 'Основное изображение',
+            title: 'Ваше изображение',
+            class: 'image-upload' // добавим класс для изображения
+        });
 
-	$("#file-upload").change(function(){
-		var file = $(this).val().split('\\').pop();
-		$("#value-file").html(file);
+        // ждем загрузки картинки браузером
+        $img.load(function () {
+            // удаляем атрибуты width и height
+            $(this).removeAttr("width")
+                .removeAttr("height")
+                .css({
+                    width: "",
+                    height: ""
+                });
+
+            // получаем  цифры размера изображения
+            var width = $(this).width();
+            var height = $(this).height();
+
+            console.log('width= ' + width + ' height= ' + height);
+            // и масштабируем его добавочным классом
+            if (width > height) {
+                $img.addClass('image-upload-w');
+                $('.wraper__image-bg').css({
+                    'height': '' + Math.round(height * 653 / width) + 'px',
+                    'width': '653px'
+                });
+            } else {
+                $img.addClass('image-upload-h');
+                $('.wraper__image-bg').css({
+                    'height': '534px',
+                    'width': '' + Math.round(width * 534 / height) + 'px'
+                });
+            }
+        });
+
+        // добавляем изображение в документ
+        $(".wraper__image-bg").append($img);
+
+    });
+
+    // событие отправки #watermark на сервер
+    $('#watermark').fileupload({
+        dataType: 'json',
+        add: function (e, data) { // отправляем картинку на сервер
+            data.submit();
+            console.log('отправляем #watermark на сервер');
+        }
+    }).bind('fileuploaddone', function (e, data) {
+
+        // проверим есть ли фон, если да загрузим ватемарк
+        if ($('.wraper__image').length > 0) {
+
+            // удаляем предыдущий вотемарк
+            if ($('#drag').length > 0) {
+                $('#drag').remove();
+            }
+            // создаем элемент изображения
+            var $wtm = $('<img>', {
+                src: $("#watermark").attr("data-url") + "files/" + data.files[0].name, // путь из данных атрибута добачной папки и имя файла из инпута
+                alt: 'Основное изображение',
+                title: 'Ваше изображение',
+                id: 'drag',
+                class: 'image-watermark ui-widget ui-widget-content' // добавим класс для изображения
+            });
+
+
+            // добавляем изображение в документ
+
+            $(".wraper__image-bg").append($wtm);
+            //$wtm.wrap('<div id="drag"></div>');
+            $("#drag").draggable({
+                containment: ".image-upload",
+                scroll: false
+            });
+        }
+
+    });
+
+
+
+    $("#file-upload").change(function () {
+        var file = $(this).val().split('\\').pop();
+        $("#value-file").html(file);
     })
 
-	$("#watermark").change(function(){
-		var file = $(this).val().split('\\').pop();
-		$("#value-watermark").html(file);
-	})
+    $("#watermark").change(function () {
+        var file = $(this).val().split('\\').pop();
+        $("#value-watermark").html(file);
+    })
 
-   
+    sliderRange.on("slidechange", function (event, ui) {
+        $(".generate__preview").fadeTo(200, (1 - ui.value / 100));
+    });
+
 
 });
