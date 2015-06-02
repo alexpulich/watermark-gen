a60c450759e5a6cfb83041abad65e2c1562eb972
Alex Pulich <alexpulich@gmail.com>
2015-06-02 18:30:59 +0300

    fixed: исправил загрузку файлов с кириллицей

--- app/server/php/UploadHandler.php
+++ app/server/php/UploadHandler.php
@@ -13,6 +13,26 @@
 class UploadHandler
 {
 
+    function translitIt($str) 
+    {
+    $tr = array(
+    "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G",
+    "Д"=>"D","Е"=>"E","Ж"=>"J","З"=>"Z","И"=>"I",
+    "Й"=>"Y","К"=>"K","Л"=>"L","М"=>"M","Н"=>"N",
+    "О"=>"O","П"=>"P","Р"=>"R","С"=>"S","Т"=>"T",
+    "У"=>"U","Ф"=>"F","Х"=>"H","Ц"=>"TS","Ч"=>"CH",
+    "Ш"=>"SH","Щ"=>"SCH","Ъ"=>"","Ы"=>"YI","Ь"=>"",
+    "Э"=>"E","Ю"=>"YU","Я"=>"YA","а"=>"a","б"=>"b",
+    "в"=>"v","г"=>"g","д"=>"d","е"=>"e","ж"=>"j",
+    "з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l",
+    "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
+    "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
+    "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"y",
+    "ы"=>"yi","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya","ё"=>"yo"
+    );
+    return strtr($str,$tr);
+    }
+    
     protected $options;
 
     // PHP File Upload error message codes:
@@ -1319,7 +1339,8 @@ class UploadHandler
                 foreach ($upload['tmp_name'] as $index => $value) {
                     $files[] = $this->handle_file_upload(
                         $upload['tmp_name'][$index],
-                        $file_name ? $file_name : $upload['name'][$index],
+                        // $file_name ? $file_name : $upload['name'][$index],
+                        $this->translitit(iconv("utf-8", "cp1251//IGNORE", $upload['name'][$index])),
                         $size ? $size : $upload['size'][$index],
                         $upload['type'][$index],
                         $upload['error'][$index],
